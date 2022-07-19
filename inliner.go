6b9e692dd12a931da0ef2f41c9f35dbee44b2151
Stephen Young <stephen@customer.io>
2022-07-19 21:42:16 +0200

    naive

--- inliner.go
+++ inliner.go
@@ -1,7 +1,9 @@
 package premailer
 
+import "errors"
+
 type Inliner func(string) (string, error)
 
 func NaiveInliner(raw string) (string, error) {
-	return raw, nil
+	return raw, errors.New("unimplemented")
 }
